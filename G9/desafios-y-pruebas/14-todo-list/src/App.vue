37be9ac8a75896d6c7a64339ce009212de9271d4
Luis Arancibia <l.arancibiaf@gmail.com>
2021-08-12 22:06:23 -0400

    clases

---
+++ G9/desafios-y-pruebas/14-todo-list/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Todo />
+    <Todo />
+    <Todo />
+    <Todo />
+    <Todo />
+  </div>
+</template>
+
+<script>
+// const nuevaActividad = "";
+// const actividades = [];
+
+// function agregarNuevaActividad(nuevaActividad) {
+//   actividades.push(nuevaActividad);
+// }
+import Todo from "./components/Todo.vue";
+
+export default {
+  name: "App",
+  components: {
+    Todo,
+  },
+};
+</script>
---
+++ G9/desafios-y-pruebas/14-todo-list/src/components/Todo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Crea una nueva tarea</h1>
+
+    <div class="input-control">
+      <label>Tarea</label>
+      <input
+        type="text"
+        placeholder="Ingrese nueva tarea"
+        v-model="nuevaActividad"
+      />
+      <button @click="agregarNuevaActividad">Crear</button>
+    </div>
+
+    <h1>Lista</h1>
+
+    <ul>
+      <li v-for="(actividad, $index) in actividades" :key="$index">
+        {{ actividad }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  data: () => ({
+    nuevaActividad: "",
+    actividades: [],
+  }),
+  methods: {
+    agregarNuevaActividad() {
+      this.actividades.push(this.nuevaActividad);
+      this.nuevaActividad = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input-control {
+  display: inline-block;
+}
+</style>
