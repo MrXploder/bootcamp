479e85615466a81e97390376826f891d1e747d49
Luis Arancibia <l.arancibiaf@gmail.com>
2022-05-11 22:40:09 -0400

    clases

---
+++ G14/experimentos-en-clases/class-and-style-binding/src/components/TodoOl.vue
@@ -0,0 +1,32 @@
+<template>
+  <ol :style="{ backgroundColor: dark ? 'black' : 'white' }">
+    <li
+      v-for="(tarea, $index) in value"
+      :key="$index"
+      class="text-center"
+      :class="{
+        'bg-high': tarea.prioridad === 'alta',
+        'bg-middle': tarea.prioridad === 'media',
+        'bg-low': tarea.prioridad === 'baja',
+      }"
+      :style="{ color: dark ? 'white' : 'black' }"
+    >
+      {{ tarea.text }} - prioridad: {{ tarea.prioridad }}
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  props: {
+    value: { type: Array, required: true },
+    dark: { type: Boolean, default: false },
+  },
+};
+</script>
+
+<style scoped>
+.text-center {
+  text-align: center;
+}
+</style>
