435855496169ee524d09be66f5d37ab2b165ebe7
Luis Arancibia <l.arancibiaf@gmail.com>
2022-06-07 10:39:24 -0400

    clases

---
+++ G14/experimentos-en-clases/simple-vue-app/src/components/SaBtn.vue
@@ -0,0 +1,20 @@
+<template>
+  <button type="button" class="btn" :class="[colorClass]">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    color: { type: String, default: 'primary' },
+  },
+  computed: {
+    colorClass() {
+      return `btn-${this.color}`;
+    },
+  },
+};
+</script>
+
+<style></style>
